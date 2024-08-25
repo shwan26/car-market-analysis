4b384e9f3b907202247892fd0e30de4d81755569
Shwan <syanshwan.26@gmail.com>
2024-08-25 20:37:40 +0700

    Update Readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 // App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import HighlightedCars from './pages/HighlightedCars';
 import CarList from './pages/CarList';
 
 const App = () => {
   return (
-    <Router basename="/car-market/">
+    <Router path="/car-market/">
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/highlighted-cars" element={<HighlightedCars />} />
